87f0d19d7107a648df23a43c3a46a89d20365627
sizsan <sizsan2001@gmail.com>
2020-01-19 13:56:53 +1000

    authenitcation complete

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const User = new Schema({
-	email: {
-		type: String,
-		required: true
-	}
-})
+    email: {
+        type: String,
+        required: true
+    }
+});
 
 // plugin the passport-local-mongoose middleware with our User schema
 User.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", User)
+module.exports = mongoose.model('User', User);
